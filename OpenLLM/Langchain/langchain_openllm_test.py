2a46d2b61d94c41a31110b14b5134250a9e40245
Caillard Mathias <mathias.caillard@telecom-sudparis.eu>
2023-12-01 13:15:08 +0100

    update test example

--- OpenLLM/Langchain/langchain_openllm_test.py
+++ OpenLLM/Langchain/langchain_openllm_test.py
@@ -1,16 +1,17 @@
 # pip install "git+https://github.com/bentoml/langchain.git@chore/migrate-to-new-api#subdirectory=libs/langchain"
 # make sure to launch an openllm server before : openllm start <your model>
 # then launch this file : CUDA_VISIBLE_DEVICES=0 python3 langchain_openllm_test.py
-# works with : 
+# works with :
 # - OpenLLM version: 0.4.34
-# - Langchain version : 0.0.339rc3
+# - Langchain version : 0.0.339rc3 (fork of Langchain)
 # launch
 
 
 from langchain.llms import OpenLLM
 import torch
-torch.FloatTensor(0).to('cuda')
+torch.FloatTensor(0).to('cuda') #To change depending of the GPU you are using
 server_url = "http://localhost:3000"
+input = "What is the difference between a cat and a dog ?"
 llm = OpenLLM(server_url=server_url)
-output = llm.generate(['tell me the difference between a duck and a goose.'])
-print(output)
+(print(llm.invoke(input)))     
+
