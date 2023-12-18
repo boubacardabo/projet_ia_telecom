c416b2c9a5a1a189d820d94b7adffd0033756465
boubacardabo <boubacar@modjo.ai>
2023-12-18 09:58:57 +0100

    wip

--- backend/llm/llm_model.py
+++ backend/llm/llm_model.py
@@ -5,19 +5,22 @@ import torch
 
 class LlmModel:
     def __init__(self, model_name=opt_350_model):
-        self.model = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-        self.tokenizer = AutoModelForCausalLM.from_pretrained(model_name)
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model_name, trust_remote_code=True
+        )
+        self.model = AutoModelForCausalLM.from_pretrained(model_name)
 
     def generate_text(self, input_text: str):
-        input_ids = self.tokenizer([input_text], return_tensors="pt")
-        output = self.model.generate(**input_ids)  # type: ignore
+        inputs = self.tokenizer.encode(input_text, return_tensors="pt")
+        print(inputs)
+        output = self.model(**inputs)  # type: ignore
         generated_text = self.tokenizer.decode(output, skip_special_tokens=True)[0]
         return generated_text
 
     def cleanup(self):
         # Ensure any open files or connections are properly closed
         # For transformers, this might include closing the model's tokenizer
-        self.tokenizer.close()
+        self.tokenizer.clean_up_tokenization_spaces()
 
         # If you're using a GPU, make sure to release GPU memory
         if self.model.device.type == "cuda":  # type: ignore
