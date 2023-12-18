3347a58abdbb7295634f4b820eeeb4347e667ec3
boubacardabo <boubacar@modjo.ai>
2023-12-18 11:01:54 +0100

    wip

--- backend/llm/llm_model.py
+++ backend/llm/llm_model.py
@@ -1,27 +1,26 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
-from llm.model_names import opt_350_model
+from llm.model_names import dolly_model, mistral_model
 import torch
 
 
 class LlmModel:
-    def __init__(self, model_name=opt_350_model):
+    def __init__(self, model_name=mistral_model):
         self.tokenizer = AutoTokenizer.from_pretrained(
             model_name, trust_remote_code=True
         )
         self.model = AutoModelForCausalLM.from_pretrained(model_name)
 
     def generate_text(self, input_text: str):
-        inputs = self.tokenizer.encode(input_text, return_tensors="pt")
-        print(inputs)
-        output = self.model.generate(inputs)  # type: ignore
-        generated_text = self.tokenizer.decode(output, skip_special_tokens=True)[0]
+        inputs = self.tokenizer.encode(
+            text=input_text,
+            return_tensors="pt",
+            add_special_tokens=False,
+        )
+        output = self.model.generate(inputs, max_new_tokens=2000)  # type: ignore
+        generated_text = self.tokenizer.decode(output[0], skip_special_tokens=True)
         return generated_text
 
     def cleanup(self):
-        # Ensure any open files or connections are properly closed
-        # For transformers, this might include closing the model's tokenizer
-        self.tokenizer.clean_up_tokenization_spaces()
-
         # If you're using a GPU, make sure to release GPU memory
         if self.model.device.type == "cuda":  # type: ignore
             self.model = self.model.to("cpu")  # Move model to CPU to release GPU memory
