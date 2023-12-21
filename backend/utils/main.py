4075251f1516c8a9b5c3a2cb6fc641870f7cdfc2
boubacardabo <boubacar@modjo.ai>
2023-12-21 02:03:41 +0100

    wip

--- backend/utils/main.py
+++ backend/utils/main.py
@@ -5,15 +5,14 @@ def select_gpu():
     user_input = input(
         "Enter GPU instance (use comma for multiple GPUs, e.g., '0' or '0,1'): "
     )
-    gpu_numbers = [int(gpu.strip()) for gpu in user_input.split(",")]
-    return gpu_numbers
+    return user_input
 
 
 def initialize_gpu(gpu_numbers):
     if torch.cuda.is_available():
-        devices = [f"cuda:{gpu}" for gpu in gpu_numbers]
-        print(f"Using GPU(s): {', '.join(map(str, gpu_numbers))}")
+        devices = f"cuda:{gpu_numbers}"
+        print(f"Using GPU(s): {gpu_numbers}")
     else:
-        devices = ["cpu"]
+        devices = "cpu"
         print("No GPU available, using CPU.")
     return devices
