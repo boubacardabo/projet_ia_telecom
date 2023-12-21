8e8da77f27c13031fe507712b2f3c76f17463474
boubacardabo <boubacar@modjo.ai>
2023-12-21 01:56:15 +0100

    wip

--- backend/test/main.py
+++ backend/test/main.py
@@ -2,33 +2,40 @@ import os
 import sys
 import torch
 
+
 backend_folder = f"{os.getcwd()}/backend"
 sys.path.append(backend_folder)
 
 
 from llm.llm_model import LlmModel
 from langchain_wrapper.lang_wrapper import LangWrapper
-
-user_input = input("Enter GPU instance: ")
-
-try:
-    gpuNumber = int(user_input)
-    torch.FloatTensor(1).to(f"cuda:{gpuNumber}")
-    model = LlmModel()
-    langchain_wrapper = LangWrapper(model=model)
-    generated_text = langchain_wrapper.invoke_llm_chain(
-        "",
-        """
-        write a c++ function to interface with gpio pins of a raspberry pi. 
-        GPIO0 adn GPIO1 are LEDs. Make them blink at a two seconds interval
-        """,
-    )
-    print(generated_text)
-
-    langchain_wrapper.cleanup()
-
-except ValueError:
-    print("Invalid input. Please enter a valid integer.")
+from utils.main import select_gpu, initialize_gpu
 
 
-# Clean up
+def main():
+    gpu_numbers = select_gpu()
+
+    try:
+        devices = initialize_gpu(gpu_numbers)
+        for device in devices:
+            torch.FloatTensor(1).to(device)
+
+        model = LlmModel()
+        langchain_wrapper = LangWrapper(model=model)
+        generated_text = langchain_wrapper.invoke_llm_chain(
+            "",
+            """
+            write a c++ function to interface with gpio pins of a raspberry pi. 
+            GPIO0 adn GPIO1 are LEDs. Make them blink at a two seconds interval
+            """,
+        )
+        print(generated_text)
+
+        langchain_wrapper.cleanup()
+
+    except ValueError:
+        print("Invalid input. Please enter valid GPU instance(s).")
+
+
+if __name__ == "__main__":
+    main()
