a596feb75347c02c0b3eb4ddc18576d4520509a8
boubacardabo <boubacar@modjo.ai>
2023-12-21 01:35:11 +0100

    langchain_wrapper

--- backend/test/main.py
+++ backend/test/main.py
@@ -1,16 +1,34 @@
+import os
+import sys
 import torch
 
-torch.FloatTensor(1).to("cuda")
-import sys
+backend_folder = f"{os.getcwd()}/backend"
+sys.path.append(backend_folder)
 
-# sys.path.append("/Users/boubacardabo/Desktop/school/projet_ia_telecom/backend")
-sys.path.append("/home/infres/bdabo/projet_ia_telecom/backend")
 
 from llm.llm_model import LlmModel
+from langchain_wrapper.lang_wrapper import LangWrapper
 
-llm = LlmModel()
-generated_text = llm.generate_text("Hello,")
-print("Generated text:", generated_text)
+user_input = input("Enter GPU instance: ")
+
+try:
+    gpuNumber = int(user_input)
+    torch.FloatTensor(1).to(f"cuda:{gpuNumber}")
+    model = LlmModel()
+    langchain_wrapper = LangWrapper(model=model)
+    generated_text = langchain_wrapper.invoke_llm_chain(
+        "",
+        """
+        write a c++ function to interface with gpio pins of a raspberry pi. 
+        GPIO0 adn GPIO1 are LEDs. Make them blink at a two seconds interval
+        """,
+    )
+    print(generated_text)
+
+    langchain_wrapper.cleanup()
+
+except ValueError:
+    print("Invalid input. Please enter a valid integer.")
+
 
 # Clean up
-llm.cleanup()
