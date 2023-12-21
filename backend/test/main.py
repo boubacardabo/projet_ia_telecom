82878ed7b68c3f2398ff090a827afc7567237170
boubacardabo <boubacar@modjo.ai>
2023-12-21 02:21:11 +0100

    wip

--- backend/test/main.py
+++ backend/test/main.py
@@ -9,10 +9,14 @@ sys.path.append(backend_folder)
 
 from llm.llm_model import LlmModel
 from langchain_wrapper.lang_wrapper import LangWrapper
+from utils.main import select_gpu, initialize_gpu
 
 
 def main():
+    gpu_numbers = select_gpu()
+
     try:
+        devices = initialize_gpu(gpu_numbers)
         torch.FloatTensor(2).to("cuda")
 
         model = LlmModel()
