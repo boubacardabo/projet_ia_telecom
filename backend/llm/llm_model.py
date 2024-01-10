22d3f63525badcabf7a611f3ff2e12b988c31eb7
boubacardabo <boubacar@modjo.ai>
2024-01-10 11:56:44 +0100

    wow

--- backend/llm/llm_model.py
+++ backend/llm/llm_model.py
@@ -13,7 +13,7 @@ class LlmModel:
             task="text-generation",
             model=self.model,
             tokenizer=self.tokenizer,
-            device_map="auto",
+            device=0
             max_new_tokens=2048,
         )
 
