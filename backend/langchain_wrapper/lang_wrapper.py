15ace47301f37314180e1ea8e4f9edb93df089d4
boubacardabo <boubacar@modjo.ai>
2024-01-10 17:00:30 +0100

    ff

--- backend/langchain_wrapper/lang_wrapper.py
+++ backend/langchain_wrapper/lang_wrapper.py
@@ -51,7 +51,7 @@ class LangWrapper:
             input={"context": context, "question": question}
         )
         if isinstance(self.llmChain, LLMChain):
-            return response["text"]
+            return response
         else:
             return response
 
