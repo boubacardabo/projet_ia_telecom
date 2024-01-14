730cf10e8b884ea3ac73a336b2a1c4c9b9d72782
boubacardabo <boubacar@modjo.ai>
2024-01-14 17:46:01 +0100

    remove unused gpu setup

--- backend/test/main.py
+++ backend/test/main.py
@@ -13,33 +13,8 @@ from langchain_wrapper.lang_wrapper import LangWrapper
 from llm.model_names import mistral_model, orca_2_model
 
 
-def select_gpu():
-    user_input = input(
-        "Enter GPU instance (use comma for multiple GPUs, e.g., '0' or '0,1'): "
-    )
-    gpu_numbers = [int(gpu.strip()) for gpu in user_input.split(",")]
-    return gpu_numbers
-
-
-def initialize_gpu(gpu_numbers):
-    if torch.cuda.is_available():
-        devices = [f"cuda:{gpu}" for gpu in gpu_numbers]
-        print(f"Using GPU(s): {', '.join(map(str, gpu_numbers))}")
-    else:
-        devices = ["cpu"]
-        print("No GPU available, using CPU.")
-
-    return devices
-
-
 def main():
-    gpu_numbers = select_gpu()
-
     try:
-        devices = initialize_gpu(gpu_numbers)
-        for device in devices:
-            torch.FloatTensor(1).to(device)
-
         model = LlmModel(model_name=orca_2_model)
         langchain_wrapper = LangWrapper(model=model)
         context = ""
