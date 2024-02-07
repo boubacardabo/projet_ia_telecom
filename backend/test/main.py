dc582d90344f87b80df9d9d739a1f81aadf933ec
boubacardabo <boubacar@modjo.ai>
2024-02-07 16:25:52 +0100

    debug

--- backend/test/main.py
+++ backend/test/main.py
@@ -27,8 +27,12 @@ def main():
 
         # langchain
         langchain_wrapper = LangWrapper(model=model)
+        print("here")
         langchain_wrapper.add_rag_wrapper(ragWrapper)
+        print("here")
         langchain_wrapper.setup_rag_llm_chain()
+        print("here")
+
         question = """
             Briefly tell me what the codegen.py file does
             """
