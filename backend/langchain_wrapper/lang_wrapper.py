6f59d3c30235ea064922255a288120661158a92c
boubacardabo <boubacar@modjo.ai>
2024-02-07 20:39:16 +0100

    okay

--- backend/langchain_wrapper/lang_wrapper.py
+++ backend/langchain_wrapper/lang_wrapper.py
@@ -92,7 +92,6 @@ class LangWrapper:
             question_generator=primary_chain,
             combine_docs_chain=combine_docs_chain,
             response_if_no_docs_found="The information needed was not found in any file",
-            get_chat_history=lambda h: h,  # type: ignore
         )
 
     def cleanup(self):
