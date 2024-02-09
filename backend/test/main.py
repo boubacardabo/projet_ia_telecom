e44baccbd9d8a5cb51f1dd79ebd28509c9283129
Caillard Mathias <mathias.caillard@telecom-sudparis.eu>
2024-02-09 18:38:41 +0100

    OpenLLM comptability

--- backend/test/main.py
+++ backend/test/main.py
@@ -1,45 +1,13 @@
 import os
 import sys
-import torch
-
-
 backend_folder = f"{os.getcwd()}/backend"
-
 sys.path.append(backend_folder)
-
-
-from llm.llm_model import LlmModel
 from embedding.rag_wrapper import RagWrapper
-from langchain_wrapper.lang_wrapper import LangWrapper
-from llm.model_names import code_llama_model_13b_instruct, mistral_model, starcoder
-from utils.main import select_gpu_if_available
-
-model_name = starcoder
-
-def move_tensors_to_gpu(obj):
-    if isinstance(obj, torch.Tensor):
-        if obj.device.type != 'cuda':
-            obj = obj.to('cuda')
-    if hasattr(obj, '__dict__'):
-        for attr_name in obj.__dict__:
-            attr = getattr(obj, attr_name)
-            setattr(obj, attr_name, move_tensors_to_gpu(attr))
-    if hasattr(obj, '__iter__'):
-        if isinstance(obj, dict):
-            for key, value in obj.items():
-                obj[key] = move_tensors_to_gpu(value)
-        else:
-            for i, item in enumerate(obj):
-                obj[i] = move_tensors_to_gpu(item)
-    return obj
-
 
 
 def main():
-    while True:
-        # model
-        model = LlmModel(model_name=model_name)
-        print("model device after init :", model.model.hf_device_map)
+    try:
+
 
         # rag
         repo_url = "https://github.com/esphome/esphome"
@@ -47,39 +15,107 @@ def main():
         file_type = ".py"
         ragWrapper = RagWrapper(repo_url=repo_url, branch=branch, file_type=file_type)
 
-        # langchain
-        langchain_wrapper = LangWrapper(model=model)
-        langchain_wrapper.add_rag_wrapper(ragWrapper)
-        langchain_wrapper.setup_rag_llm_chain()
+        
 
 
-        #move_tensors_to_gpu(langchain_wrapper)
+        choice = input("Choose HuggingFacePipeline ('h') or OpenLLM ('o'): ").lower().strip()
+        if choice == 'h':
 
-        question = """
-            Briefly tell me what the codegen.py file does
-            """
-        generated_text = langchain_wrapper.invoke_llm_chain(question)
-        history = generated_text["chat_history"]  # type: ignore
-        # gen_text = model.generate_text(question)
-        print(generated_text["answer"])  # type: ignore
+            from llm.llm_model import LlmModel
 
-        question = """
-            output EXATCLY the COMPLETE code of 'iter_components' function AS IS
-            """
-        generated_text = langchain_wrapper.invoke_llm_chain(question=question)
+            from langchain_wrapper.lang_wrapper import LangWrapper
+            from llm.model_names import code_llama_model_13b_instruct
 
-        # gen_text = model.generate_text(question)
-        print(generated_text["answer"])  # type: ignore
+            # model
+            model_name = code_llama_model_13b_instruct
+            model = LlmModel(model_name=model_name)
 
-        question = """
-            what is the path of a02yyuw.cpp file in the repository ?
-            """
-        generated_text = langchain_wrapper.invoke_llm_chain(question=question)
 
-        # gen_text = model.generate_text(question)
-        print(generated_text["answer"])  # type: ignore
 
-        langchain_wrapper.cleanup()
+            # langchain
+            langchain_wrapper = LangWrapper(model=model)
+            langchain_wrapper.add_rag_wrapper(ragWrapper)
+            langchain_wrapper.setup_rag_llm_chain()
+
+            question = """
+                Briefly tell me what the codegen.py file does
+                """
+            generated_text = langchain_wrapper.invoke_llm_chain(question)
+            history = generated_text["chat_history"]  # type: ignore
+            # gen_text = model.generate_text(question)
+            print(generated_text["answer"])  # type: ignore
+
+            question = """
+                output EXATCLY the COMPLETE code of 'iter_components' function AS IS
+                """
+            generated_text = langchain_wrapper.invoke_llm_chain(question=question)
+
+            # gen_text = model.generate_text(question)
+            print(generated_text["answer"])  # type: ignore
+
+            question = """
+                what is the path of a02yyuw.cpp file in the repository ?
+                """
+            generated_text = langchain_wrapper.invoke_llm_chain(question=question)
+
+            # gen_text = model.generate_text(question)
+            print(generated_text["answer"])  # type: ignore
+
+            langchain_wrapper.cleanup()
+
+
+
+
+            
+        elif choice == 'o':
+
+            print("OpenLLM")
+
+
+            from langchain_community.llms import OpenLLM
+            from langchain.prompts import PromptTemplate
+            from langchain.chains.question_answering import load_qa_chain
+            from langchain_community.llms import OpenLLM
+
+
+            server_url = "http://localhost:3000"
+            llm = OpenLLM(server_url=server_url)
+
+
+            # Prompt
+            template = """Use the following pieces of context to answer the question at the end. 
+            If you don't know the answer, just say that you don't know, don't try to make up an answer. 
+            Keep the answer as concise as possible. 
+            {context}
+            Question: {question}
+            Helpful Answer:"""
+            QA_CHAIN_PROMPT = PromptTemplate(
+                input_variables=["context", "question"],
+                template=template,
+            )
+
+            # Docs
+            question = "Write a unit test for the gpio_base_schema method, based on the specification of gpio_base_schema."
+            docs = ragWrapper.retriever.get_relevant_documents(question)
+
+            # Chain
+            chain = load_qa_chain(llm, chain_type="stuff", prompt=QA_CHAIN_PROMPT)
+            output = chain.invoke({"input_documents": docs, "question": question}, return_only_outputs=True)
+            print(output['output_text'])
+
+
+
+
+
+
+
+        else:
+            print("Invalid choice. Exiting.")
+            return
+            
+
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
