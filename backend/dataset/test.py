aab3d34e5400f41b2fa9161cd251a22261043cc3
Caillard Mathias <mathias.caillard@telecom-sudparis.eu>
2024-02-19 15:10:06 +0100

    fixing bug + comments + test

--- backend/dataset/test.py
+++ backend/dataset/test.py
@@ -9,17 +9,8 @@ if path_to_remove in sys.path:
     sys.path.remove(path_to_remove)
 
 
-from dataset import dataset
-from utils.main import write_function_to_file2
 
 
-id = 8
-function_string_whole = dataset[id][ 'whole_func_string']
-func_code_url = dataset[id]["func_code_url"]
-print(func_code_url)
 
-write_function_to_file2(function_string_whole, backend_folder + "\\code_writer_usecase_dataset\\functions.py")
-
-file_path = backend_folder + "\\code_writer_usecase_dataset\\functions.py"
 
 
