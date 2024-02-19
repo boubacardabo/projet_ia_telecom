823ece50272913b01a36ef067b46c5dd6cf5cd7a
Caillard Mathias <mathias.caillard@telecom-sudparis.eu>
2024-02-19 15:10:06 +0100

    fixing bug + comments + test

--- backend/code_writer_usecase/functions.py
+++ backend/code_writer_usecase/functions.py
@@ -1,7 +1,6 @@
-function_string = """
-```python
+
 def calculate_rectangle_area(length, width):
-    \"\"\"
+    """
     Calculates the area of a rectangle.
 
     Parameters:
@@ -10,9 +9,7 @@ def calculate_rectangle_area(length, width):
 
     Returns:
         float: The area of the rectangle.
-    \"\"\"
+    """
     if length <= 0 or width <= 0:
         raise ValueError("Length and width must be positive numbers")
     return length * width
-```
-"""
--- backend/code_writer_usecase_dataset/main.py
+++ backend/code_writer_usecase_dataset/main.py
@@ -1,11 +1,11 @@
 import os
 import sys
 
-backend_folder = f"{os.getcwd()}\\backend"
+backend_folder = f"{os.getcwd()}/backend"
 if backend_folder not in sys.path:
     sys.path.append(backend_folder)
 
-path_to_remove = f"{os.getcwd()}\\backend\\code_writer_usecase_dataset"
+path_to_remove = f"{os.getcwd()}/backend/code_writer_usecase_dataset"
 if path_to_remove in sys.path:
     sys.path.remove(path_to_remove)
 
@@ -16,6 +16,7 @@ os.environ["LANGCHAIN_TRACING_V2"] = 'true'
 os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
 os.environ["LANGCHAIN_API_KEY"] = "ls__a21c5d9069a442c08645e82f0a7330cc"
 os.environ["LANGCHAIN_PROJECT"]= "PRIM-NXP"
+os.environ["TAVILY_API_KEY"]= "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"
 
 from embedding.rag_wrapper import RagWrapper
 from langchain_wrapper.lang_wrapper import LangWrapper
@@ -95,12 +96,74 @@ def main():
 
 
         ####
+        from dataset import dataset
+        from utils.main import write_function_to_file2
 
 
- 
+        id = 8
+        function_string_whole = dataset[id][ 'whole_func_string']
+        func_code_url = dataset[id]["func_code_url"]
+
+        # write_function_to_file2(function_string_whole, backend_folder + "\\code_writer_usecase_dataset\\functions.py")
 
 
-        ###
+        os.environ["TAVILY_API_KEY"]= "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"
+        from langchain_community.tools.tavily_search import TavilySearchResults
+        search = TavilySearchResults()
+
+
+        from langchain.text_splitter import RecursiveCharacterTextSplitter
+        from langchain_community.document_loaders import WebBaseLoader
+        from langchain_community.vectorstores import FAISS
+        from langchain_community.embeddings import HuggingFaceEmbeddings
+        from embedding.model_names import all_MiniLM_L6_v2
+
+        loader = WebBaseLoader(func_code_url)
+        docs = loader.load()
+        documents = RecursiveCharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=200
+        ).split_documents(docs)
+
+
+        model_name = all_MiniLM_L6_v2
+
+
+        embeddings = HuggingFaceEmbeddings(
+            model_name=model_name,
+            encode_kwargs={"normalize_embeddings": True},
+            model_kwargs={"device": "cuda"},
+                    )
+        vector = FAISS.from_documents(documents, embeddings)
+        retriever = vector.as_retriever()
+
+
+        from langchain.tools.retriever import create_retriever_tool
+
+        retriever_tool = create_retriever_tool(
+            retriever,
+            "langsmith_search",
+            "Search for information about a function.",
+        )
+
+        tools = [search, retriever_tool]
+
+        from langchain import hub
+
+        # Get the prompt to use - you can modify this!
+        prompt = hub.pull("hwchase17/openai-functions-agent")
+        from langchain.agents import create_openai_functions_agent
+
+        agent = create_openai_functions_agent(llm, tools, prompt)
+
+        from langchain.agents import AgentExecutor
+
+        agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+        print(agent_executor.invoke({"input": "What are the modules and function depending of face_locations?"}))
+
+
+
+        ####
 
 
 
