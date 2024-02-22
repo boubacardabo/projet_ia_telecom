f983adba1aae6b52aa048ff8632d7dc75a5be103
Caillard Mathias <mathias.caillard@telecom-sudparis.eu>
2024-02-22 18:56:52 +0100

    .env password + id for faster connection

--- frontend/home.py
+++ frontend/home.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import paramiko
 import traceback
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv(dotenv_path=os.path.join(os.getcwd(), ".env"))  
 
 port = 22  # SSH
 
@@ -41,13 +46,19 @@ def execute_ssh_command(client, command):
 # Streamlit app
 st.title("GPU access")
 
+
+# Get default values from environment variables or use fallback values
+default_username = os.getenv("SSH_USERNAME", "username")
+default_password = os.getenv("SSH_PASSWORD", "")
+default_langsmith_api_key = os.getenv("LANGCHAIN_API_KEY", "")
+
 # Sidebar inputs for SSH connection parameters
 with st.sidebar:
     st.subheader("SSH Connection Parameters")
     hostname = st.text_input("Hostname", value="gpu6.enst.fr")
-    username = st.text_input("Username", value="username")
-    password = st.text_input("Password", type="password")
-    langsmith_api_key = st.text_input("Langsmith API Key", key="langchain_search_api_key_langsmith", type="password")
+    username = st.text_input("Username", value=default_username)
+    password = st.text_input("Password", type="password", value=default_password)
+    langsmith_api_key = st.text_input("Langsmith API Key", key="langchain_search_api_key_langsmith", type="password", value=default_langsmith_api_key)
 
 # Connect to SSH server (establish SSH connection only once)
 client = establish_ssh_connection(hostname, port, username, password)
