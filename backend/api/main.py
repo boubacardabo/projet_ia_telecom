6a9eef06207eea01967fe95f263d00df3d5589da
Boubacar Dabo <bdabo@gpu4.enst.fr>
2024-02-27 21:37:12 +0100

    app back

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,1 +1,24 @@
+import os
+import sys
 
+backend_folder = f"{os.path.expanduser('~')}/projet_ia_telecom/backend"
+sys.path.append(backend_folder)
+
+from fastapi import FastAPI, Response
+from llm.llm_model import LlmModel
+from llm.model_names import mistral_model
+import uvicorn
+
+
+app = FastAPI()
+LlmModel(model_name=mistral_model)
+
+
+@app.get("/")
+async def root():
+    # Customize the response message as needed
+    return Response("FastAPI app is running!")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=7000)
